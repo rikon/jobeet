121bd3cf5459e8fe48f3fa6b957e765c030e0bde
ri_kon <ri_kon@comsq.com>
2015-07-14 11:52:31 +0900

    a

--- src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -144,6 +144,7 @@ class JobControllerTest extends WebTestCase
     	
     	//preview you job's test
     	$form = $crawler->selectButton('Preview your job')->form(array(
+    			'job[type]'		=> 'part-time',
     			'job[company]'	=> 'Sensio Labs',
     			'job[url]'		=> 'http://www.sensio.com/',
     			'job[file]'		=> __DIR__.'/../../../../../web/bundles/ibwjobeet/images/sensio-labs.gif',
@@ -159,17 +160,18 @@ class JobControllerTest extends WebTestCase
     	
     	
     	//TODO:??????
-    	//$client->followRedirect();
-    	//$this->assertEquals('Ibw\JobeetBundle\Controller\JobController::previewAction', $client->getRequest()->attributes->get('_controller'));
+    	$client->followRedirect();
+    	$this->assertEquals('Ibw\JobeetBundle\Controller\JobController::previewAction', $client->getRequest()->attributes->get('_controller'));
     	
     	
-    	//
+    	//レコードが存在することをテスト
+    	/*
     	$kernel = static::createKernel();
     	$kernel->boot();
     	$em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
-    	
-    	$query = $em->createQuery('SELECT count(j.id) FROM IbwJobeetBundle:Job j WHERE j.location=:location AND j.is_activated IS NOT NULL AND j.is_public!=0');
+    	$query = $em->createQuery('SELECT count(j.id) FROM IbwJobeetBundle:Job j WHERE j.location=:location AND j.is_activated IS NULL AND j.is_public=0');
     	$query->setParameter('location', 'Paris, France');
     	$this->assertTrue(0 < $query->getSingleScalarResult());
+    	*/
     }
 }
