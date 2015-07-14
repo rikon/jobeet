db1ce049e1a4ff8fd7e20ee30b669dba8ab5c33b
ri_kon <ri_kon@comsq.com>
2015-07-14 11:53:26 +0900

    あ

--- src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
+++ src/Ibw/JobeetBundle/Tests/Controller/JobControllerTest.php
@@ -159,19 +159,17 @@ class JobControllerTest extends WebTestCase
     	$this->assertEquals('Ibw\JobeetBundle\Controller\JobController::createAction', $client->getRequest()->attributes->get('_controller'));
     	
     	
-    	//TODO:??????
+    	//TODO:ジョブ生成
     	$client->followRedirect();
     	$this->assertEquals('Ibw\JobeetBundle\Controller\JobController::previewAction', $client->getRequest()->attributes->get('_controller'));
     	
     	
     	//レコードが存在することをテスト
-    	/*
     	$kernel = static::createKernel();
     	$kernel->boot();
     	$em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
     	$query = $em->createQuery('SELECT count(j.id) FROM IbwJobeetBundle:Job j WHERE j.location=:location AND j.is_activated IS NULL AND j.is_public=0');
     	$query->setParameter('location', 'Paris, France');
     	$this->assertTrue(0 < $query->getSingleScalarResult());
-    	*/
     }
 }
