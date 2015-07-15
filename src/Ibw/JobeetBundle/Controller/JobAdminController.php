3a8ba50472d83e4101bcf42c161262510c6ec4e1
ri_kon <ri_kon@comsq.com>
2015-07-15 13:24:01 +0900

    aaa

--- src/Ibw/JobeetBundle/Controller/JobAdminController.php
+++ src/Ibw/JobeetBundle/Controller/JobAdminController.php
@@ -6,4 +6,30 @@ use Sonata\AdminBundle\Controller\CRUDController as Controller;
 class JobAdminController extends Controller
 {
     // Your code will be here
+    public function batchActionExtend(ProxyQueryInterface $selectedModelQuery)
+    {
+        if ($this->admin->isGranted('EDIT') === false || $this->admin->isGranted('DELETE') === false) {
+            throw new AccessDeniedException();
+        }
+
+        $modelManager = $this->admin->getModelManager();
+
+        $selectedModels = $selectedModelQuery->execute();
+
+        try {
+            foreach ($selectedModels as $selectedModel) {
+                $selectedModel->extend();
+                $modelManager->update($selectedModel);
+            }
+        } catch (\Exception $e) {
+            $this->get('session')->getFlashBag()->add('sonata_flash_error', $e->getMessage());
+
+            return new RedirectResponse($this->admin->generateUrl('list',$this->admin->getFilterParameters()));
+        }
+
+        $this->get('session')->getFlashBag()->add('sonata_flash_success',  sprintf('The selected jobs validity has been extended until %s.', date('m/d/Y', time() + 86400 * 30)));
+
+        return new RedirectResponse($this->admin->generateUrl('list',$this->admin->getFilterParameters()));
+    }
+
 }
