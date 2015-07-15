e58dd8f5ac31984479804e52b7716902e4d774e1
ri_kon <ri_kon@comsq.com>
2015-07-15 15:12:10 +0900

    a

---
+++ src/Ibw/JobeetBundle/Command/JobeetUsersCommand.php
@@ -0,0 +1,47 @@
+<?php
+namespace Ibw\JobeetBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Ibw\JobeetBundle\Entity\User;
+
+
+class JobeetUsersCommand extends ContainerAwareCommand
+{
+	public function configure()
+	{
+		$this->setName('ibw:jobeet:users')
+			->setDescription('Add Jobeet users')
+			->addArgument('username', InputArgument::REQUIRED, 'The username')
+			->addArgument('password', InputArgument::REQUIRED, 'The password');
+	}
+	
+	
+	public function execute(InputInterface $input, OutputInterface $output) {
+		
+		$username = $input->getArgument('username');
+		$password = $input->getArgument('password');
+		
+		$em = $this->getContainer()->get('doctrine')->getManager();
+		
+		$user = new User();
+		$user->setUsername($username);
+		
+		//encode password
+		$factory = $this->getContainer()->get('security.encoder_factory');
+		$encoder = $factory->getEncoder($user);
+		$encodePassword = $encoder->encodePassword($password, $user->getSalt());
+		$user->setPassword($encodePassword);
+		$em->persist($user);
+		$em->flush(9);
+		
+		$output->writeln(sprintf('Added %s user with password %s', $username, $password));
+
+)
+		
+		//$user->setPassword($password);
+		
+	}
+}
