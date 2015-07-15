0c7811c11cff49453eeeec6318888c2220cc7aad
ri_kon <ri_kon@comsq.com>
2015-07-15 12:41:36 +0900

    a

---
+++ src/Ibw/JobeetBundle/Admin/CategoryAdmin.php
@@ -0,0 +1,13 @@
+<?php
+namespace Ibw\JobeetBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class CategoryAdmin extends Admin
+{
+    // Your code will be here
+}
---
+++ src/Ibw/JobeetBundle/Admin/JobAdmin.php
@@ -0,0 +1,15 @@
+<?php
+namespace Ibw\JobeetBundle\Admin;
+
+use Sonata\AdminBundle\Admin\Admin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Validator\ErrorElement;
+use Sonata\AdminBundle\Form\FormMapper;
+use Sonata\AdminBundle\Show\ShowMapper;
+use Ibw\JobeetBundle\Entity\Job;
+
+class JobAdmin extends Admin
+{
+    // Your code will be here
+}
