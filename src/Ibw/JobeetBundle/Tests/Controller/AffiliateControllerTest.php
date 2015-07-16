2f4d80ba94da78253d05de338cf0304ea55d6cc6
ri_kon <ri_kon@comsq.com>
2015-07-16 14:20:15 +0900

    a

--- src/Ibw/JobeetBundle/Tests/Controller/AffiliateControllerTest.php
+++ src/Ibw/JobeetBundle/Tests/Controller/AffiliateControllerTest.php
@@ -73,38 +73,35 @@ class AffiliateControllerTest extends WebTestCase
 	{
 		$client = static::createClient();
 		$crawler = $client->request('GET', '/affiliate/new');
+		
 		$this->assertEquals('Ibw\JobeetBundle\Controller\AffiliateController::newAction', $client->getRequest()->attributes->get('_controller'));
 		
-		
 		$form = $crawler->selectButton('Submit')->form(array(
-			'affiliate[url]' => 'http://sensio-labs.com/',
-			'affiliate[email]' => 'jobeet@example.com',
+				'affiliate[url]' => 'http://sensio-labs.com/',
+				'affiliate[email]' => 'jobeet@example.com'
 		));
+		
 		$client->submit($form);
 		$this->assertEquals('Ibw\JobeetBundle\Controller\AffiliateController::createAction', $client->getRequest()->attributes->get('_controller'));
-		
-		
 		
 		$kernel = static::createKernel();
 		$kernel->boot();
 		$em = $kernel->getContainer()->get('doctrine.orm.entity_manager');
 		
-		$query = $em->createQuery('SELECT count(a.email) FROM IbwJobeetBundle:Affiliate a WHERE a.email=:email');
+		$query = $em->createQuery('SELECT count(a.email) FROM IbwJobeetBundle:Affiliate a WHERE a.email = :email');
 		$query->setParameter('email', 'jobeet@example.com');
 		$this->assertEquals(1, $query->getSingleScalarResult());
 		
-		
 		$crawler = $client->request('GET', '/affiliate/new');
 		$form = $crawler->selectButton('Submit')->form(array(
-			'affiliate[url]' => 'http://sensio-labs.com/',
-			'affiliate[email]' => 'not.an.email',
+				'affiliate[email]'        => 'not.an.email',
 		));
 		$crawler = $client->submit($form);
 		
-		//check if we have 1 errors
+		// check if we have 1 errors
 		$this->assertTrue($crawler->filter('.error_list')->count() == 1);
-		//check if we have error onaffliate_email field.
-		//$this->assertTrue($crawler->filter('#affiliate_email')->siblings()->first()->filter('.error_list')->count() == 1);
+		// check if we have error on affiliate_email field
+		$this->assertTrue($crawler->filter('#affiliate_email')->siblings()->first()->filter('.error_list')->count() == 1);
 	}
 	
 	
