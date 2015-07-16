d1ff8ba1cb1a5ea78402438b4609d6796893d1f6
ri_kon <ri_kon@comsq.com>
2015-07-16 13:15:08 +0900

    a

--- src/Ibw/JobeetBundle/Controller/AffiliateController.php
+++ src/Ibw/JobeetBundle/Controller/AffiliateController.php
@@ -5,6 +5,7 @@ namespace Ibw\JobeetBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Ibw\JobeetBundle\Entity\Affiliate;
 use Ibw\JobeetBundle\Form\AffiliateType;
+use Symfony\Component\HttpFoundation\Request;
 
 
 class AffiliateController extends Controller
@@ -19,6 +20,41 @@ class AffiliateController extends Controller
 			'entity' => $entity,
 			'form' => $form->createView(),
 		));
+	}
+	
+	public function createAction(Request $request)
+	{
+		$affiliate = new Affiliate();
+		$form = $this->createForm(new AffiliateType(), $affiliate);
+		$form->handleRequest($request);
+		$em = $this->getDoctrine()->getManager();
+		
+		if($form->isValid()) {
+			
+			/*
+			$formData = $request->get('affiliate');
+			$affiliate->setUrl($formData['url']);
+			$affiliate->setEmail($formData['email']);
+			$affiliate->setIsActive(false);
+			*/
+			
+			$em->persist($affiliate);
+			$em->flush();
+			
+			return $this->redirect($this->generateUrl('ibw_affiliate_wait'));
+		}
+		
+		return $this->render('IbwJobeetBunlde:Affiliate:affiliate_new.html.twig', array(
+				'entity' => $affiliate,
+				'form' => $form->createView()
+		));
+		
+	}
+	
+	
+	public function waitAction()
+	{
+		return $this->render('IbwJobeetBundle:Affiliate:wati.html.twig');	
 	}
 	
 }
