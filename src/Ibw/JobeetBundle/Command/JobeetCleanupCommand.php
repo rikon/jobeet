726bf5cf74c376c7d8ed2eced1ec0698580fc0ad
ri_kon <ri_kon@comsq.com>
2015-07-17 15:50:19 +0900

    a

--- src/Ibw/JobeetBundle/Command/JobeetCleanupCommand.php
+++ src/Ibw/JobeetBundle/Command/JobeetCleanupCommand.php
@@ -5,6 +5,7 @@ use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Ibw\JobeetBundle\Entity\Job;
 
 
 class JobeetCleanupCommand extends ContainerAwareCommand
@@ -21,8 +22,26 @@ class JobeetCleanupCommand extends ContainerAwareCommand
 	{
 		$days = $input->getArgument('days');
 		$em = $this->getContainer()->get('doctrine')->getManager();
-		$nb = $em->getRepository('IbwJobeetBundle:Job')->cleanup($days);
 		
+		//clean up Lucene index
+		$index = Job::getLuceneIndex();
+		$q = $em->getRepository('IbwJobeetBundle:Job')->createQueryBuilder('j')
+				->where('j.expires_at < :date')
+				->setParameter('date', date('Y-m-d'))
+				->getQuery();
+		
+		$jobs = $q->getResult();
+		foreach ($jobs as $job) {
+			if($hit = $index->find('pk:' . $job->getId())) {
+				$index->delete($hit->id);
+			}
+		}
+		$index->optimize();
+		$output->writeln('Cleaned up and optimized the job index');
+		
+		
+		//Remoce stale jobs
+		$nb = $em->getRepository('IbwJobeetBundle:Job')->cleanup($days);
 		$output->writeln(sprintf('Removed %d stale Job.', $nb));
 	}
 }
