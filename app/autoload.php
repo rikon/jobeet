dd3e0654fe6d983f0463aeaf4b230f5cb7818a34
ri_kon <ri_kon@comsq.com>
2015-07-17 13:41:27 +0900

    a

--- app/autoload.php
+++ app/autoload.php
@@ -4,6 +4,7 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
 require_once __DIR__.'/../vendor/Zend/Loader/Autoloader.php';
+Zend_Loader_Autoloader::getInstance();
 
 /**
  * @var ClassLoader $loader
@@ -11,6 +12,6 @@ require_once __DIR__.'/../vendor/Zend/Loader/Autoloader.php';
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader(array($loader, 'loadClass'));
-Zend_Loader_Autoloader::getInstance();
+
 
 return $loader;
